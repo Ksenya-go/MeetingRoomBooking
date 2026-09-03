e5756bc1c70c5fdc8e6e9bb1d17a1cc76b782ebb
Ksenia-NURE <Ksenia-NURE@ACER-KSENIA>
2026-09-03 22:21:39 +0300

    Add Account and Resources controllers with Razor views

---
+++ Api/Controllers/ResourcesController.cs
@@ -0,0 +1,50 @@
+﻿using Mediator;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MeetingBooking.Application.Resources;
+
+namespace MeetingBooking.Api.Controllers;
+
+[Authorize]
+public class ResourcesController : Controller
+{
+    private readonly ISender _sender;
+
+    public ResourcesController(ISender sender)
+    {
+        _sender = sender;
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(new GetAllResourcesQuery(), cancellationToken);
+        return View(result.Value);
+    }
+
+    [Authorize(Roles = "Admin")]
+    public IActionResult Create() => View();
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(CreateResourceCommand command, 
+        CancellationToken cancellationToken)
+    {
+        var result = await _sender.Send(command, cancellationToken);
+        if (result.IsFailed)
+        {
+            ModelState.AddModelError(string.Empty, string.Join("; ", result.Errors));
+            return View(command);
+        }
+        return RedirectToAction(nameof(Index));
+    }
+
+    [Authorize(Roles = "Admin")]
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Delete(Guid id, CancellationToken cancellationToken)
+    {
+        await _sender.Send(new DeleteResourceCommand(id), cancellationToken);
+        return RedirectToAction(nameof(Index));
+    }
+}
---
+++ Api/Models/RegisterViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeetingBooking.Api.Models;
+
+public class RegisterViewModel
+{
+    [Required]
+    public string FullName { get; set; } = string.Empty;
+
+    [Required, EmailAddress]
+    public string Email { get; set; } = string.Empty;
+
+    [Required, DataType(DataType.Password), MinLength(8)]
+    public string Password { get; set; } = string.Empty;
+}
